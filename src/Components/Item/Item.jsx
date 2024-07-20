86786d67b312879efb0070f3c9e42bb214a75fb2
Jijo Jacob <Jijojacob988@gmail.com>
2024-07-20 15:22:18 +0530

    product display component created and deigned

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Item.css';
+import { Link } from 'react-router-dom';
 
 const Item = ({ id, name, image, new_price, old_price }) => {
   return (
     <div className='item'>
-      <img src={image} alt={name} />
+      <Link to={`/product/${id}`}>
+        <img src={image} alt={name} />
+      </Link>
       <h2>{name}</h2>
       <div className="item-prices">
         <p className="item-price-new">${new_price.toFixed(2)}</p>
